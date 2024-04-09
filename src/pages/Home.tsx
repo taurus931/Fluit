e4b86ca7a80aa24c13e938e68e1d1f126bb1056c
tungle <lethanhtunglc5593@gmail.com>
2024-04-09 15:32:15 +0900

    create fluit component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,17 +5,7 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer />
       </IonContent>
     </IonPage>
